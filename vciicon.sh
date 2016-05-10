91c9ce5737b4e880def2d45c606b260cc14c22cc
larsenv <larsen@weborican.com>
2016-05-09 19:51:06 -0500

    Fixed mkdir

--- vciicon.sh
+++ vciicon.sh
@@ -7,7 +7,7 @@ MINPARAMS=2
 echo "VCiicon Beta by Larsenv."
 echo "Requires ImageMagick for now, and icon to iconTex.png."
 echo "\n"
-mkdir "meta"
+mkdir -p "meta"
 convert iconTex.png -resize 122x81\! -gravity center iconTex.png
 convert iconTex.png -gravity center -crop 128x128 iconTex.png
 composite -geometry +3+17 iconTex.png ./resources/iconTex.png ./resources/iconTex.png ./meta/iconTex.tga
