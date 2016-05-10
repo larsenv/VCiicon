3b81a5601b520f0224da73d4055d53da272d31ca
larsenv <larsen@weborican.com>
2016-05-09 19:37:36 -0500

    Moved the rm command to happen after composite

--- vciicon.sh
+++ vciicon.sh
@@ -4,7 +4,7 @@ echo "\n"
 mkdir "meta"
 convert iconTex.png -resize 122x81\! -gravity center iconTex.png
 convert iconTex.png -gravity center -crop 128x128 iconTex.png
-rm iconTex.png
 composite -geometry +3+17 iconTex.png ./resources/iconTex.png ./resources/iconTex.png ./meta/iconTex.tga
+rm iconTex.png
 echo "\n"
 echo "Done."
