6c33aac861c1029f53d91b55bb521ecd286c9c1f
larsenv <larsen@weborican.com>
2016-05-09 19:23:24 -0500

    Fixed the corners

--- vciicon.sh
+++ vciicon.sh
@@ -3,6 +3,6 @@ echo "Requires ImageMagick for now, and icon to iconTex.png."
 echo "\n"
 convert iconTex.png -resize 122x81\! -gravity center iconTex.png
 convert iconTex.png -gravity center -crop 128x128 iconTex.tga
-composite -geometry +3+17 iconTex.tga ./resources/iconTex.png iconTex.bmp
+composite -geometry +3+17 iconTex.tga ./resources/iconTex.png ./resources/iconTex.png iconTex.bmp
 echo "\n"
 echo "Done."
