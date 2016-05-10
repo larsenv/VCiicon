cac9a4a9ce6bc309a19853b11ebb2cf18004be39
larsenv <larsen@weborican.com>
2016-05-09 19:15:47 -0500

    Did some more work with the bash

--- vciicon.sh
+++ vciicon.sh
@@ -1,2 +1,8 @@
-echo "VCiicon v1.0 by Larsenv."
+echo "VCiicon Beta by Larsenv."
+echo "Requires ImageMagick for now, and icon to iconTex.png."
 echo "\n"
+convert iconTex.png -resize 130 -gravity center iconTex.png
+convert iconTex.png -gravity center -crop 128x128 iconTex.tga
+composite -gravity center ./resources/iconTex.png icon: iconTex.png
+echo "\n"
+echo "Done."
