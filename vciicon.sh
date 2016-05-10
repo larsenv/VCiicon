a1586286094da299a795661941fd00cf0ec44e34
larsenv <larsen@weborican.com>
2016-05-09 19:35:36 -0500

    Remove the original file

--- vciicon.sh
+++ vciicon.sh
@@ -4,6 +4,7 @@ echo "\n"
 mkdir "meta"
 convert iconTex.png -resize 122x81\! -gravity center iconTex.png
 convert iconTex.png -gravity center -crop 128x128 iconTex.png
+rm iconTex.png
 composite -geometry +3+17 iconTex.png ./resources/iconTex.png ./resources/iconTex.png ./meta/iconTex.tga
 echo "\n"
 echo "Done."
