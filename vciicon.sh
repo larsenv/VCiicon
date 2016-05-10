c77be1cb0522ce68d344d9a9926ce74a57fee75a
larsenv <larsen@weborican.com>
2016-05-10 17:21:17 -0500

    Fixed the N64 filenames

--- vciicon.sh
+++ vciicon.sh
@@ -35,17 +35,17 @@ fi
 
 if [ "$ARGUMENT" = "N64" ]
 then 
-	convert screenshot.png -resize 122x92\! -gravity center iconTex.png
-	convert iconTex.png -gravity center -crop 128x128 iconTex.png
-	composite -geometry +3+9 iconTex.png ./resources/iconTex-N64.png ./resources/iconTex-N64.png ./meta/iconTex.tga
-	rm -rf iconTex.png
-	convert screenshot.png -resize 400x300\! screenshot.png
-	convert screenshot.png \
+	convert screenshot.png -resize 122x92\! -gravity center iconTex-N64.png
+	convert iconTex-N64.png -gravity center -crop 128x128 iconTex-N64.png
+	composite -geometry +3+9 iconTex-N64.png ./resources/iconTex-N64.png ./resources/iconTex-N64.png ./meta/iconTex.tga
+	rm -rf iconTex-N64.png
+	convert screenshot.png -resize 400x300\! iconTex-N64.png
+	convert iconTex-N64.png \
      \( +clone  -alpha extract \
         -draw 'fill black polygon 0,0 0,10 10,0 fill white circle 10,10 10,0' \
         \( +clone -flip \) -compose Multiply -composite \
         \( +clone -flop \) -compose Multiply -composite \
-     \) -alpha off -compose CopyOpacity -composite screenshot.png
+     \) -alpha off -compose CopyOpacity -composite iconTex-N64.png
 fi
 
 if [ "$ARGUMENT" = "NES" ]
