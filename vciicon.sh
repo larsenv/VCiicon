490cf0b512eb0c053da83bfc740b31c247ed3dc6
larsenv <larsen@weborican.com>
2016-05-09 19:31:21 -0500

    Fixed extensions

--- vciicon.sh
+++ vciicon.sh
@@ -2,7 +2,7 @@ echo "VCiicon Beta by Larsenv."
 echo "Requires ImageMagick for now, and icon to iconTex.png."
 echo "\n"
 convert iconTex.png -resize 122x81\! -gravity center iconTex.png
-convert iconTex.png -gravity center -crop 128x128 iconTex.tga
-composite -geometry +3+17 iconTex.tga ./resources/iconTex.png ./resources/iconTex.png iconTex.bmp
+convert iconTex.png -gravity center -crop 128x128 iconTex.png
+composite -geometry +3+17 iconTex.png ./resources/iconTex.png ./resources/iconTex.png iconTex.tga
 echo "\n"
 echo "Done."
