94ab9f21a0bdc7414b23d4f72ee36ffa7139e74d
larsenv <larsen@weborican.com>
2016-05-09 19:34:14 -0500

    Made the output be saved in the meta folder

--- vciicon.sh
+++ vciicon.sh
@@ -1,8 +1,10 @@
 echo "VCiicon Beta by Larsenv."
 echo "Requires ImageMagick for now, and icon to iconTex.png."
 echo "\n"
+mkdir "meta"
 convert iconTex.png -resize 122x81\! -gravity center iconTex.png
 convert iconTex.png -gravity center -crop 128x128 iconTex.png
 composite -geometry +3+17 iconTex.png ./resources/iconTex.png ./resources/iconTex.png iconTex.tga
+mv iconTex.tga "meta"
 echo "\n"
 echo "Done."
