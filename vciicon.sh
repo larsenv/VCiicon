7583dc42fa7df4e3c7c4d4399f489f80d504ae12
larsenv <larsen@weborican.com>
2016-05-22 18:12:03 -0500

    Usage fixed

--- vciicon.sh
+++ vciicon.sh
@@ -12,11 +12,12 @@ if (( $# != 3 ))
 then
 	echo "Usage:"
 	echo "\n"
-	echo "sh vciicon.sh <name of platform> <title> <release_year>"
+	echo "sh vciicon.sh <name of platform> <title> <release_year> <release_type>"
 	echo "\n"
-	echo "Name of platform can be DS, GBA, N64, NES, PCEngine, or SNES. Required"
+	echo "Name of platform can be DS, GBA, N64, NES, PCEngine, or SNES. Required."
 	echo "Title is the title of your game. Required."
 	echo "Release Year is the year the game was released. Required."
+	echo "Release Type can be Europe (Released in Europe), USA (Released in the USA), or Worldwide (Released). This is for cases for games like Super Mario Bros, where it was released in USA and Japan in 1985. Since it was released in Europe in 1987, you can use Europe as this argument to show the specific text."
     exit 1
 fi
 
